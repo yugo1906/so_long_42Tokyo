d8bef1ea1052508f3783be2e08c92fcac1d743c5
yugo <yugo.hoshino1906@gmail.com>
2023-03-07 01:58:07 +0900

    add:minilibx.h

---
+++ minilibx.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minilibx.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 01:45:15 by yughoshi          #+#    #+#             */
+/*   Updated: 2023/03/07 01:54:45 by yughoshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINILIBX_H
+# define MINILIBX_H
+# include "./include_gnl/get_next_line.h"
+# include "./include_printf/ft_printf.h"
+# include "./include_libft/libft.h"
+#endif
