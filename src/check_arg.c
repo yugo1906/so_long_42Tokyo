e3fe9e09d6b2e02842a0e7edf31789aaea77f229
yugo <yugo.hoshino1906@gmail.com>
2023-03-16 09:41:48 +0900

    clean: マジックナンバーの2をFD_2に変更

--- src/check_arg.c
+++ src/check_arg.c
@@ -6,7 +6,7 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 09:29:42 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/03/11 01:05:44 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/03/16 09:40:43 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ void	check_arg(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		ft_putstr_fd("Error\nfile name required\n", 2);
+		ft_putstr_fd("Error\nfile name required\n", FD_2);
 		exit(EXIT_FAILURE);
 	}
 	len = ft_strlen(argv[1]);
 	if (len <= 4)
 	{
-		ft_putstr_fd("Error\nfile name is 4 characters or less\n", 2);
+		ft_putstr_fd("Error\nfile name is 4 characters or less\n", FD_2);
 		exit(EXIT_FAILURE);
 	}
 	if (ft_memcmp(&argv[1][len - 4], ".ber", 5))
 	{
-		ft_putstr_fd("Error\nfile isn`t the .ber extension\n", 2);
+		ft_putstr_fd("Error\nfile isn`t the .ber extension\n", FD_2);
 		exit(EXIT_FAILURE);
 	}
 }
