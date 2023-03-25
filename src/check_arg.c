0d5639870730d26cc300086b60ff779c40daf154
yugo <yugo.hoshino1906@gmail.com>
2023-03-25 20:10:38 +0900

    fix:エラーメッセージ

--- src/check_arg.c
+++ src/check_arg.c
@@ -6,7 +6,7 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 09:29:42 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/03/16 09:40:43 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/03/23 08:15:47 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void	check_arg(int argc, char **argv)
 	len = ft_strlen(argv[1]);
 	if (len <= 4)
 	{
-		ft_putstr_fd("Error\nfile name is 4 characters or less\n", FD_2);
+		ft_putstr_fd("Error\nFile name is 4 characters or less\n", FD_2);
 		exit(EXIT_FAILURE);
 	}
 	if (ft_memcmp(&argv[1][len - 4], ".ber", 5))
 	{
-		ft_putstr_fd("Error\nfile isn`t the .ber extension\n", FD_2);
+		ft_putstr_fd("Error\nFile isn`t the .ber extension\n", FD_2);
 		exit(EXIT_FAILURE);
 	}
 }
